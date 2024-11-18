9c73573b3fbaa6bd9e55d9d63d8d4f5f1f390cb1
Xcellentshop <141801253+Xcellentshop@users.noreply.github.com>
2024-11-18 13:54:59 -0300

    Update VehicleForm.tsx

--- src/components/VehicleForm.tsx
+++ src/components/VehicleForm.tsx
@@ -16,7 +16,7 @@ const CITIES: City[] = ['Medianeira', 'SMI', 'Missal', 'Itaipul√¢ndia', 'Serran√
 const STATES = [
   'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG',
   'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE',
-  'TO', 'EX'
+  'TO', 'EX', '--'
 ];
 
 export default function VehicleForm() {
