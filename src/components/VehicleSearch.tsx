bc288cf455364573ffa3fa7b006733dba3554376
Xcellentshop <141801253+Xcellentshop@users.noreply.github.com>
2024-11-18 13:56:13 -0300

    Update VehicleSearch.tsx

--- src/components/VehicleSearch.tsx
+++ src/components/VehicleSearch.tsx
@@ -17,7 +17,7 @@ const CITIES: City[] = ['Medianeira', 'SMI', 'Missal', 'Itaipul√¢ndia', 'Serran√
 const STATES = [
   'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG',
   'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE',
-  'TO', 'EX'
+  'TO', 'EX', '--'
 ];
 
 export default function VehicleSearch() {
