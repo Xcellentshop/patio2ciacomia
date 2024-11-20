9ce4a32c2165f59206e0a1c5669b895ef3fd95e4
Xcellentshop <141801253+Xcellentshop@users.noreply.github.com>
2024-11-20 15:05:14 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-<<<<<<< HEAD
-=======
   darkMode: 'class',
->>>>>>> 55a98a4 (Primeiro commit)
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 55a98a4 (Primeiro commit)
